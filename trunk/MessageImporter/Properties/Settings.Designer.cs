9490d9fa102f068c9e02fa0c21784aec61c739e0
michal.koscak <michal.koscak@c5a4e1dd-d58a-f3ea-4b22-1f4074e462fa>
2012-11-04 14:34:57 +0000

    Zmena nastaveni programu zo System na User, tak aby sa mohli v ramci app upravovat.

--- trunk/MessageImporter/Properties/Settings.Designer.cs
+++ trunk/MessageImporter/Properties/Settings.Designer.cs
@@ -23,102 +23,135 @@ namespace MessageImporter.Properties {
             }
         }
         
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("1234567")]
         public string ActiveStyle_ICO {
             get {
                 return ((string)(this["ActiveStyle_ICO"]));
             }
+            set {
+                this["ActiveStyle_ICO"] = value;
+            }
         }
         
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("14")]
         public int DueDateAdd {
             get {
                 return ((int)(this["DueDateAdd"]));
             }
+            set {
+                this["DueDateAdd"] = value;
+            }
         }
         
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("0200")]
         public string BankCode {
             get {
                 return ((string)(this["BankCode"]));
             }
+            set {
+                this["BankCode"] = value;
+            }
         }
         
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("0308")]
         public string ConstSym {
             get {
                 return ((string)(this["ConstSym"]));
             }
+            set {
+                this["ConstSym"] = value;
+            }
         }
         
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("VUB")]
         public string Bank {
             get {
                 return ((string)(this["Bank"]));
             }
+            set {
+                this["Bank"] = value;
+            }
         }
         
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("UD")]
         public string ClasifficationVAT {
             get {
                 return ((string)(this["ClasifficationVAT"]));
             }
+            set {
+                this["ClasifficationVAT"] = value;
+            }
         }
         
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("3")]
         public string Accounting {
             get {
                 return ((string)(this["Accounting"]));
             }
+            set {
+                this["Accounting"] = value;
+            }
         }
         
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("20")]
         public float DPH_percent {
             get {
                 return ((float)(this["DPH_percent"]));
             }
+            set {
+                this["DPH_percent"] = value;
+            }
         }
         
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("SK")]
         public string TypePrice {
             get {
                 return ((string)(this["TypePrice"]));
             }
+            set {
+                this["TypePrice"] = value;
+            }
         }
         
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("01")]
         public string Storage {
             get {
                 return ((string)(this["Storage"]));
             }
+            set {
+                this["Storage"] = value;
+            }
         }
         
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("shipping")]
         public string ShippingCode {
             get {
                 return ((string)(this["ShippingCode"]));
+            }
+            set {
+                this["ShippingCode"] = value;
             }
         }
     }
